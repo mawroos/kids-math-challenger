f197f2d5275ca36b72ff3c5b185b4c46612df172
mawroos <33208325+mawroos@users.noreply.github.com>
2025-09-06 12:10:28 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
+    
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/kids-math-challenger/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY) 
